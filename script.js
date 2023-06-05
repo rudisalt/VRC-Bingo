9be47c06f0828a0124ac9bcc372b9287d3c0092a
TakenouchiTR <62298020+TakenouchiTR@users.noreply.github.com>
2023-06-04 21:47:39 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,156 @@
+//Gets each element with the class of "bingoCell" and stores it in the variable "cells"
+var cells = document.getElementsByClassName("bingoCell");
+
+var cellTextOptions = [
+    "Top",
+    "Bottom",
+    "Overly Sexual",
+    "Certified X",
+    "Bisexual",
+    "Cuddling",
+    "Poly",
+    "Constantly Flirting",
+    "Mommy",
+    "Daddy",
+    "Mental Illness",
+    "Femboy",
+    "Weird Sexualities",
+    "Good Boy",
+    "Pronouns in Bio",
+    "Pride Flag",
+    "Gay",
+    "Infantile Speech",
+    "Overly Long Bio",
+    "Trauma Dump",
+    "Fursicution",
+    "Bear",
+    "Princess",
+    "Computer Science",
+    "Minor",
+    "Kink Avatar",
+    "Phantom Sense",
+    "Phantom Pain",
+    "Sparkle Dog",
+    "Drama",
+    "Internet Psychologist",
+    "VRC Family",
+    "Mute",
+    "Narcissistic",
+    "Licking",
+    "RP Noises",
+    "Goo / Latex / Pool Toy",
+    "Harness / BDSM",
+    "Leash",
+    "Jockstrap",
+    "Bulge",
+    "DPS Enabled",
+    "Gay as Personality",
+    "Trans as Personality",
+    "Edgy Character",
+    "Headpats",
+]
+
+var freeSpaceOptions = [
+    "Mirror Dweller",
+    "Daddy Issues",
+]
+
+var clickedCells = new Array(25).fill(false);
+
+var score = 0;
+
+var gotBingo = false;
+
+var rowScores = [0, 0, 0, 0, 0];
+var columnScores = [0, 0, 0, 0, 0];
+var diagonalScores = [0, 0];
+
+//Shuffles the array of cell text options
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+//Randomizes the text in each cell and removes the "clicked" class
+function randomizeCells() {
+    shuffle(cellTextOptions);
+    shuffle(freeSpaceOptions);
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = cellTextOptions[i];
+        cells[i].classList.remove("clicked");
+    }
+    cells[12].innerHTML = freeSpaceOptions[0] + "<br/>(Free Space)";
+}
+
+function resetGame() {
+    gotBingo = false;
+    clickedCells.fill(false);
+    rowScores.fill(0);
+    columnScores.fill(0);
+    diagonalScores.fill(0);
+    randomizeCells();
+}
+
+function updateScore(newScore) {
+    if (newScore < 0) {
+        newScore = 0;
+    }
+
+    score = newScore;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function checkBingo() {
+    if (rowScores.includes(5) || columnScores.includes(5) || diagonalScores.includes(5)) {
+        if (gotBingo) {
+            return;
+        }
+        gotBingo = true;
+        updateScore(score + 1);
+    } else {
+        if (!gotBingo) {
+            return;
+        }
+        gotBingo = false;
+        updateScore(score - 1);
+    }
+}
+
+//Hooks up the "New Card" button to the function "resetGame"
+document.getElementById("newCardButton").addEventListener("click", resetGame);
+
+//Hooks up the "Add Point" button to the function "updateScore"
+document.getElementById("addPointButton").addEventListener("click", function() { updateScore(score + 1); });
+
+//Hooks up the "Remove Point" button to the function "updateScore"
+document.getElementById("removePointButton").addEventListener("click", function() { updateScore(score - 1); });
+
+//Loops through each cell and adds an event listener to each one
+const backgroundColors = ["#F0F8FF", "#B0E0E6"];
+for (var i = 0; i < cells.length; i++) {
+    const index = i;
+    cells[index].addEventListener("click", function() {
+        //When the cell is clicked, it will toggle the class "clicked"
+        this.classList.toggle("clicked");
+        clickedCells[index] = !clickedCells[index];
+
+        //Adds or removes a point from the row, column, and diagonal that the cell is in
+        var row = Math.floor(index / 5);
+        var column = index % 5;
+        var scoreDifference = clickedCells[index] ? 1 : -1;
+        
+        rowScores[row] += scoreDifference;
+        columnScores[column] += scoreDifference;
+        if (row == column) {
+            diagonalScores[0] += scoreDifference;
+        }
+        if (row + column == 4) {
+            diagonalScores[1] += scoreDifference;
+        }
+
+        checkBingo();
+    });
+
+    cells[index].style.backgroundColor = backgroundColors[index % 2];
+}
+
+resetGame();
